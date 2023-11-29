c7bc6b99c32a46f4c85ad3c396e300110eb8c6b2
Nikita <mikita.verkhavodka@gmail.com>
2023-11-29 11:30:14 +0300

    add profile menu

--- Frontend/apgular-app-16/src/app/components/profile-menu/profile-menu.component.ts
+++ Frontend/apgular-app-16/src/app/components/profile-menu/profile-menu.component.ts
@@ -8,7 +8,46 @@ import { Component } from '@angular/core';
 export class ProfileMenuComponent {
   newLogin: string = '';
   newEmail: string = '';
+  errorMessageUsername = '';
+  errorMessagePassword = '';
+  errorMessageEmail = '';
   // Методы для обработки событий
+
+
+  isUsernameValid(field: string): boolean {
+    const value = this[field as keyof ProfileMenuComponent];
+    if (value.length > 20) {
+      this.errorMessageUsername = 'Username must be less than 20 characters';
+      return true;
+    }
+    if (value.length < 4) {
+      this.errorMessageUsername = 'Username must be more than 4 characters';
+      return true;
+    }
+    const isValid = !(value === '' || value === undefined || value === null);
+    if (!isValid) {
+      this.errorMessageUsername = 'Username is required';
+    } else {
+      this.errorMessageUsername = '';
+    }
+    return !isValid;
+  }
+
+
+  isEmailValid(field: string): boolean {
+    const value = this[field as keyof ProfileMenuComponent];
+    if (value.length > 100) {
+      this.errorMessagePassword = 'Email must be less than 100 characters';
+      return true;
+    }
+    const isValid = !(value === '' || value === undefined || value === null);
+    if (!isValid) {
+      this.errorMessageEmail = 'Email is required';
+    } else {
+      this.errorMessageEmail = '';
+    }
+    return !isValid;
+  }
 
   editLogin() {
   }
